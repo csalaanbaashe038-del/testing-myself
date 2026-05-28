c55dc0f3b5de715d6701df025235b5b60832010b
Hkam Hosseen <csalaanbaashe038@gmail.com>
2026-05-28 03:18:35 -0700

    landing page , and I test myself

---
+++ index.js
@@ -0,0 +1,6 @@
+alert("Welcome to my page");
+
+let button=document.getElementById("btn");
+button.addEventListener('click', function(){
+    alert("Enjoy my work, while I achieved it with hard work, consistency, and perseverance.");
+});
